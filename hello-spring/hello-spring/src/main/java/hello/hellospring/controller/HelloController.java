b7924b34bdf718b2324d95266e077666014bcb7f
yookyung Choi <ykc131719@gmail.com>
2021-05-18 15:50:20 +0900

    JSON API

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -30,4 +31,29 @@ public class HelloController {
     // localhost:8080/hello-mvc => 내장 톰켓 서버
     // 1. helloController : GetMapping 있으면 실행 -> hello-template.html 실행
     // 2. hello-template.html : HTML 화면 출력
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;  //이때는 html 태그 없이 문자만 출력됨
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //Json 형태
+
+    }
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+    }
 }
