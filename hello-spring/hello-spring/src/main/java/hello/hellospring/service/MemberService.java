d93e1435555ceb783779f09487e221e6561bcc32
yookyung Choi <ykc131719@gmail.com>
2021-05-18 21:05:36 +0900

    Test Member Service

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private  final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     //회원가입
     public Long join(Member member){
